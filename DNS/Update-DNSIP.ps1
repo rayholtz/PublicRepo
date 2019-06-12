d4966c057b0069befb98affb23a300d273809f61
Ray Holtz <ray@rayholtz.com>
2019-06-12 11:44:31 -0500

    >6/12/2019 v2 - Changed IP verification, made variables mandatory parameters

--- DNS/Update-DNSIP.ps1
+++ DNS/Update-DNSIP.ps1
@@ -3,39 +3,42 @@
     This script was written by Ray Holtz on 6/12/2019
 #>
 
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$HostName,
+    [Parameter(Mandatory = $true)]
+    [string]$ZoneName,    
+    [Parameter(Mandatory = $true)]
+    [string]$DNSserver,    
+    [Parameter(Mandatory = $true)]
+    [string]$NewIP
+)
+
+<#
 $HostName = Read-Host -Prompt "Host to update IP for?"
 $ZoneName = Read-Host -Prompt "What zone to modify record in? (domain name)"
+$DNSserver = Read-Host -Prompt "Which DNS Server should this change be made on?"
+#>
+$pattern = "^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$"
 
 do {
-    $NewIP = Read-Host -Prompt "What IP address to change to?"
-
-    try {
-        [IPAddress] $NewIP > $null 2>&1
+#   $NewIP = Read-Host -Prompt "What IP address to change to?"
+    if ($NewIP -match $pattern) {
         $r = 1
-    }
-    catch {
+    } else {
         Write-Host "That was not a vaild IP address.  Please Try Again" -BackgroundColor DarkYellow -ForegroundColor DarkRed
         $r = 0
+        $NewIP = Read-Host -Prompt "What IP address to change to?"
     }
 
 } until ( $r -eq "1" )
 
-
-#$NewObj = Get-DnsServerResourceRecord -Name $HostNAme -ZoneName $ZoneName -RRType "A"
-#$OldObj = Get-DnsServerResourceRecord -Name $HostNAme -ZoneName $ZoneName -RRType "A"
-#$NewObj.RecordData  = $ipaddress
+$NewObj = Get-DnsServerResourceRecord -Name $HostName -ZoneName $ZoneName -RRType "A" -ComputerName $DNSserver
+$OldObj = Get-DnsServerResourceRecord -Name $HostName -ZoneName $ZoneName -RRType "A" -ComputerName $DNSserver
 
 #$NewObj.TimeToLive = [System.TimeSpan]::FromHours(2)
 
 $NewObj.RecordData.ipv4address=[System.Net.IPAddress]::parse($NewIP)
-Set-DnsServerResourceRecord -NewInputObject $NewObj -OldInputObject $OldObj -ZoneName $ZoneName -PassThru
+Set-DnsServerResourceRecord -NewInputObject $NewObj -OldInputObject $OldObj -ZoneName $ZoneName -PassThru -ComputerName $DNSserver
 
-
-Write-host "$HostName.$ZoneName is now $NewIP"
-
-
-
-
-
-
-
+#$NewObj|Format-List
